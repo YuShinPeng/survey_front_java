b4dc91d83dff463c39e4bee1d96e9685ea14351f
YuShinPeng <ccsauce173@gmail.com>
2023-05-30 22:09:25 +0800

    first commit

---
+++ src/main/java/com/example/survey_front_java/SurveyFrontJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.survey_front_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyFrontJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyFrontJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/survey_front_java/SurveyFrontJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.survey_front_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyFrontJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
